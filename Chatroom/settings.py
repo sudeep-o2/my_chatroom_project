8cac262ecaed3404508a054e9388395d77c59e60
Sudeep <sudeepsavai16@gmail.com>
2023-05-30 18:30:42 +0530

    vercel setting

--- Chatroom/settings.py
+++ Chatroom/settings.py
@@ -127,11 +127,9 @@ STATIC_URL = 'static/'
 
 import os
 
-STATICFILES_DIRS=[
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT=BASE_DIR / 'static/images'
 
